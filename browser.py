8bc639ba4ea2e1ca05f75760bc7f4d365667e8c9
Zephyrus02 <aneeshraskar@gmail.com>
2023-01-09 09:29:28 +0530

    Added functionality like naviagte and url bar to browser

--- browser.py
+++ browser.py
@@ -1,12 +1,51 @@
-import sys
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWidgets import QApplication, QLineEdit, QMainWindow, QPushButton
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-app = QApplication(sys.argv)
 
-view = QWebEngineView()
-view.load(QUrl("https:/www.google.com"))
-view.show()
+class Browser(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
 
-sys.exit(app.exec_())
+        # Create the QWebEngineView
+        self.view = QWebEngineView(self)
+        self.view.load(QUrl("http://www.google.com"))
+        self.setCentralWidget(self.view)
+
+        # Create the URL bar
+        self.url_bar = QLineEdit(self)
+        self.url_bar.setText("http://www.google.com")
+        self.url_bar.setGeometry(0, 0, 400, 30)
+        self.url_bar.setFocus()
+
+        # Create the back and forward buttons
+        self.back_button = QPushButton("<", self)
+        self.back_button.setGeometry(0, 30, 30, 30)
+        self.forward_button = QPushButton(">", self)
+        self.forward_button.setGeometry(30, 30, 30, 30)
+
+        # Connect the buttons to their respective slots
+        self.back_button.clicked.connect(self.view.back)
+        self.forward_button.clicked.connect(self.view.forward)
+
+        # Connect the URL bar to its slot
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+
+        # Connect the view's urlChanged signal to the url_bar's setText slot
+        self.view.urlChanged.connect(self.update_url_bar)
+
+    def navigate_to_url(self):
+        q = QUrl(self.url_bar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+        self.view.setUrl(q)
+
+    def update_url_bar(self, q):
+        self.url_bar.setText(q.toString())
+        self.url_bar.setCursorPosition(0)
+
+
+app = QApplication([])
+window = Browser()
+window.show()
+app.exec_()
